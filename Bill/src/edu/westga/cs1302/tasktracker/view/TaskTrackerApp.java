938e53f36caef6ff0f1677754fad19a52aa6b567
bellarusso <Irusso1@my.westga.edu>
2025-09-22 18:49:09 -0400

    checkstyle for TaskTracker
    added correct indention
    added java doc comments for methods

--- Bill/src/edu/westga/cs1302/tasktracker/view/TaskTrackerApp.java
+++ Bill/src/edu/westga/cs1302/tasktracker/view/TaskTrackerApp.java
@@ -5,18 +5,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main entry point for the Task Tracker application.
+ *
+ * <p>This class extends {@link javafx.application.Application} and is responsible
+ * for launching the JavaFX application, loading the main window FXML, and
+ * displaying the primary stage.
+ */
 public class TaskTrackerApp extends Application {
 
-    @Override
+  @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
-        Scene scene = new Scene(loader.load());
-        primaryStage.setTitle("Task Tracker");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+    Scene scene = new Scene(loader.load());
+    primaryStage.setTitle("Task Tracker");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+  
+  /**
+   * The main method launches the JavaFX application.
+   *
+   * @param args command-line arguments (not used)
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
