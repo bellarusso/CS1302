d9b2651a016cb1545f74663fd9dfcb1e0512faf2
bellarusso <Irusso1@my.westga.edu>
2025-09-22 18:53:21 -0400

    Checkstyle for Task.java
    implemented java doc comments for methods
    fixed indention

--- Bill/src/edu/westga/cs1302/tasktracker/model/Task.java
+++ Bill/src/edu/westga/cs1302/tasktracker/model/Task.java
@@ -1,34 +1,47 @@
 package edu.westga.cs1302.tasktracker.model;
 
+/**
+ * Represents a task with a name, description, and priority.
+ *
+ * <p>Tasks are immutable for name and priority, but the description can be updated.
+ */
 public class Task {
-    private final String name;
-    private final String priority;
-    private String description;
+  private final String name;
+  private final String priority;
+  private String description;
 
-    public Task(String name, String description, String priority) {
-        this.name = name;
-        this.description = description;
-        this.priority = priority;
-    }
+  /**
+   * Constructs a new Task with the given name, description, and priority.
+   *
+   * @param name the name of the task
+   * @param description the task description
+   * @param priority the task priority
+   */
+  
+  public Task(String name, String description, String priority) {
+    this.name = name;
+    this.description = description;
+    this.priority = priority;
+  }
 
-    public String getName() {
-        return this.name;
-    }
+  public String getName() {
+    return this.name;
+  }
 
-    public String getPriority() {
-        return this.priority;
-    }
+  public String getPriority() {
+    return this.priority;
+  }
 
-    public String getDescription() {
-        return this.description;
-    }
+  public String getDescription() {
+    return this.description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    @Override
+  @Override
     public String toString() {
-        return this.name;
-    }
+    return this.name;
+  }
 }
