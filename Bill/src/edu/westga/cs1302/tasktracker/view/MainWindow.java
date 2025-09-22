79822183d8ed4210c372a7aca3462cfb785a38d8
bellarusso <Irusso1@my.westga.edu>
2025-09-22 15:54:51 -0400

    1C -Add MainWindow controller
    Added mainWindow class with textField, textArea, comboBox, and listView for tasks.
    Implemented addTask() method and intialized priority comboBox

--- Bill/src/edu/westga/cs1302/tasktracker/view/MainWindow.java
+++ Bill/src/edu/westga/cs1302/tasktracker/view/MainWindow.java
@@ -1,5 +1,43 @@
 package edu.westga.cs1302.tasktracker.view;
 
+import edu.westga.cs1302.tasktracker.model.Task;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class MainWindow {
 
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextArea descriptionField;
+    @FXML
+    private ComboBox<String> priorityBox;
+    @FXML
+    private ListView<Task> taskList;
+
+    private ObservableList<Task> tasks;
+
+    public void initialize() {
+        tasks = FXCollections.observableArrayList();
+        taskList.setItems(tasks);
+
+        priorityBox.setItems(FXCollections.observableArrayList("Low", "Medium", "High"));
+    }
+
+    @FXML
+    private void addTask() {
+        String name = nameField.getText();
+        String desc = descriptionField.getText();
+        String priority = priorityBox.getValue();
+
+        if (name != null && !name.isEmpty() && priority != null) {
+            Task newTask = new Task(name, desc, priority);
+            tasks.add(newTask);
+            nameField.clear();
+            descriptionField.clear();
+            priorityBox.getSelectionModel().clearSelection();
+        }
+    }
 }
