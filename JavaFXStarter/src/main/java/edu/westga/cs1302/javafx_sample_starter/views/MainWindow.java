ddc013abbf2f80c4dd760eec87724903f8ccc034
bellarusso <Irusso1@my.westga.edu>
2025-09-25 20:54:29 -0400

    Added @FXML fields and controller for subtask A
    deeclared textfield and area, comboBox, and listView
    intialized comboBox with low, medium and high
    implemented handleAddTaskButton to create task objects
    added input validation and warning alerts for missing name or priority

--- JavaFXStarter/src/main/java/edu/westga/cs1302/javafx_sample_starter/views/MainWindow.java
+++ JavaFXStarter/src/main/java/edu/westga/cs1302/javafx_sample_starter/views/MainWindow.java
@@ -1,18 +1,71 @@
 package edu.westga.cs1302.javafx_sample_starter.views;
 
+import edu.westga.cs1302.javafx_sample_starter.model.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * Controller class for drawing various things to our canvas window.
- * 
+ *
  * @author CS 1302
  * @version Fall 2025
  */
 public class MainWindow {
-    
-    /**
-     * Perform any needed initialization of UI components and underlying objects.
-     */
-    public void initialize() {
-    	
+
+  @FXML
+  private TextField nameTextField;
+
+  @FXML
+  private TextArea descriptionTextArea;
+
+  @FXML
+  private ComboBox<String> priorityComboBox;
+
+  @FXML
+  private ListView<Task> taskListView;
+
+  /**
+   * Perform any needed initialization of UI components and underlying objects.
+   */
+  public void initialize() {
+    priorityComboBox.getItems().addAll("Low", "Medium", "High");
+  }
+
+  /**
+   * Method to handle the Add Task button being pressed. Creates task and adds to
+   * listView
+   */
+  @FXML
+  private void handleAddTaskButton() {
+    String name = nameTextField.getText();
+    String description = descriptionTextArea.getText();
+    String priority = priorityComboBox.getValue();
+
+    if (name == null || name.trim().isEmpty()) {
+      showAlert("Must enter name");
+      return;
     }
+    if (priority == null) {
+      showAlert("Must select priority");
+      return;
+    }
+    Task newTask = new Task(name, description, priority);
+    taskListView.getItems().add(newTask);
+
+    nameTextField.clear();
+    descriptionTextArea.clear();
+    priorityComboBox.getSelectionModel().clearSelection();
+  }
+
+  private void showAlert(String message) {
+    Alert alert = new Alert(AlertType.WARNING);
+    alert.setTitle("Input Error");
+    alert.setContentText(message);
+    alert.showAndWait();
+  }
 }
